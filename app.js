2babb870588f273918165d222f58dac2b6e54d58
Adres Felipe Trujillo Madrigal <Adres Felipe Trujillo Madrigal>
2017-01-23 17:55:06 +0100

    Pagina de Bienvenida

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-var app = angular.module('MyPetShopApp',[]);
+  var app = angular.module('MyPetShopApp',[]);
+
+  app.controller('WelcomeController', function($scope){
+    $scope.title = 'MyPetShopApp';
+    $scope.welcomeMsg = 'Hi welcome to my pet shop! We <3 pets, Enjoy :)';
+  });
